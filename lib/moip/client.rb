2970fb8f9aab72d8fcfe7a093fa96a82eac96c75
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-12-07 20:41:59 -0200

    removing puts

--- lib/moip/client.rb
+++ lib/moip/client.rb
@@ -39,8 +39,6 @@ module MoIP
       # Envia uma instrução para pagamento único
       def checkout(attributes = {})
         body = DirectPayment.body(attributes)
-        puts "************ XML ************"
-        puts body
         full_data = peform_action!(:post, 'EnviarInstrucao/Unica', :body => body)
        # raise full_data.inspect
         get_response!(full_data["EnviarInstrucaoUnicaResponse"]["Resposta"])
